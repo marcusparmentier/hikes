c6cc4d0dfc118600248de6b24e6d240e23a1c0b9
Daniel Cheng and Marcus Parmentier <marcus@>
2017-09-12 10:59:41 -0700

    Add toggle feature to sections 2 and 4 using js and add css for cursor.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#secHeader2").click(function() {
+    $("#hideSection2").toggle();
+  });
+  $("#secHeader4").click(function() {
+    $("#hideSection4").toggle();
+  });
+});
